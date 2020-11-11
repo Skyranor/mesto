8f41449c915e5ab69430f77675b050b34cd44caf
Dmitry Baryshau <dima.baryshev@live.com>
2020-11-11 04:31:30 +0300

    fix: change the script for adding text to the form

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,49 +5,51 @@ let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
 
+// выбираем элементы формы
+let form = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
+
 // выбираем имя и информацию о себе в поле для ввода
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-
-// выбираем элементы формы
-let form = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type-name');
-let jobInput = document.querySelector('.popup__input_type-description');
+//добавляем имя и информацию о себе в поле для ввода
+nameInput.value = profileName.textContent;
+jobInput.value = profileDescription.textContent;
 
 // функция открытия попапа
 function showPopup() {
   popup.classList.add('popup_opened');
-  popup.removeEventListener('click', showPopup)
+  popup.removeEventListener('click', showPopup);
 }
 
 // функция закрытия попапа
 function closePopup() {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
 }
 
 // Обработчик клика на попап
 function popupClickHandler(event) {
   if (event.target.classList.contains('popup')) {
-    closePopup()
+    closePopup();
   }
 }
 
-
+// Обработчик «отправки» формы
 function submitForm(event) {
   // запрет перезагрузки страницы
   event.preventDefault();
-
-  // ввод новых значений в поля input
+  //добавление новых значений
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
+  //закрытие попапа
   closePopup();
 }
 
 // слушатели событий
 editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup)
+popupCloseButton.addEventListener('click', closePopup);
 popup.addEventListener('mousedown', popupClickHandler);
 form.addEventListener('submit', submitForm);
-
 
